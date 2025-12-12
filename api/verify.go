244ace5da6b4368a1f85ccbef6aa41371c048209
ryanchristo <12519942+ryanchristo@users.noreply.github.com>
2025-12-12 08:18:01 -0800

    refactor: types and consistent names

--- api/verify.go
+++ api/verify.go
@@ -1,11 +1,14 @@
 package handler
 
 import (
+	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"math/big"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -33,7 +36,7 @@ func Verify(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	// Unmarshal the request body
+	// Decode the request body
 	var requestBody RequestBody
 	err = json.NewDecoder(r.Body).Decode(&requestBody)
 	if err != nil {
@@ -52,14 +55,26 @@ func Verify(w http.ResponseWriter, r *http.Request) {
 	// Check the payment version
 	if requestBody.X402Version == 1 {
 
+		// Check the payment payload and requirements scheme
+		if paymentPayload.Scheme != paymentRequirements.Scheme {
+			http.Error(w, "payment scheme does not match requirements scheme", http.StatusBadRequest)
+			return
+		}
+
+		// Check the payment payload and requirements network
+		if paymentPayload.Network != paymentRequirements.Network {
+			http.Error(w, "payment network does not match requirements network", http.StatusBadRequest)
+			return
+		}
+
 		// Check the payment scheme
 		if paymentPayload.Scheme == "exact" {
 
 			// Check the payment network
-			if paymentPayload.Network == "eip155:11155111" {
+			if paymentPayload.Network == "sepolia" {
 
 				// Verify the payment on Sepolia
-				result, err := verifyV1ExactSepolia(paymentPayload, paymentRequirements)
+				result, err := verifyV1ExactSepolia(paymentPayload.Payload, paymentRequirements)
 				if err != nil {
 					var se utils.StatusError
 					if errors.As(err, &se) {
@@ -111,29 +126,41 @@ type RequestBody struct {
 
 // PaymentPayload is the payment payload.
 type PaymentPayload struct {
-	Scheme        string        `json:"scheme"`
-	Network       string        `json:"network"`
-	Authorization Authorization `json:"authorization"`
+	Scheme  string  `json:"scheme"`
+	Network string  `json:"network"`
+	Payload Payload `json:"payload"`
+}
+
+// Payload is the payload for the payment.
+type Payload struct {
 	Signature     string        `json:"signature"`
+	Authorization Authorization `json:"authorization"`
 }
 
 // Authorization is the authorization for the payment.
 type Authorization struct {
-	To          string `json:"to"`
 	From        string `json:"from"`
-	Value       string `json:"value"`
-	ValidAfter  string `json:"validAfter"`
-	ValidBefore string `json:"validBefore"`
+	To          string `json:"to"`
+	Value       int64  `json:"value"`
+	ValidAfter  int64  `json:"validAfter"`
+	ValidBefore int64  `json:"validBefore"`
 	Nonce       string `json:"nonce"`
 }
 
 // PaymentRequirements is the payment requirements.
 type PaymentRequirements struct {
-	Amount        string `json:"amount"`
-	Recipient     string `json:"recipient"`
-	AssetName     string `json:"assetName"`
-	AssetVersion  string `json:"assetVersion"`
-	AssetContract string `json:"assetContract"`
+	Scheme            string `json:"scheme"`
+	Network           string `json:"network"`
+	MaxAmountRequired int64  `json:"maxAmountRequired"`
+	Asset             string `json:"asset"`
+	PayTo             string `json:"payTo"`
+	Extra             Extra  `json:"extra"`
+}
+
+// Extra is the extra for the payment.
+type Extra struct {
+	Name    string `json:"assetName"`
+	Version string `json:"assetVersion"`
 }
 
 // VerifyResult is the result of the verification.
@@ -142,12 +169,12 @@ type VerifyResult struct {
 	Reason string `json:"reason"`
 }
 
-func verifyV1ExactSepolia(p PaymentPayload, r PaymentRequirements) (VerifyResult, error) {
+func verifyV1ExactSepolia(p Payload, r PaymentRequirements) (VerifyResult, error) {
 
 	now := time.Now()
 
 	// Verify the authorization valid after time is in the past
-	validAfter, err := time.Parse(time.RFC3339, p.Authorization.ValidAfter)
+	validAfter, err := time.Parse(time.RFC3339, strconv.FormatInt(p.Authorization.ValidAfter, 10))
 	if err != nil {
 		return VerifyResult{}, utils.NewStatusError(
 			errors.New("invalid valid after time"),
@@ -162,7 +189,7 @@ func verifyV1ExactSepolia(p PaymentPayload, r PaymentRequirements) (VerifyResult
 	}
 
 	// Verify the authorization valid before time is in the future
-	validBefore, err := time.Parse(time.RFC3339, p.Authorization.ValidBefore)
+	validBefore, err := time.Parse(time.RFC3339, strconv.FormatInt(p.Authorization.ValidBefore, 10))
 	if err != nil {
 		return VerifyResult{}, utils.NewStatusError(
 			errors.New("invalid valid before time"),
@@ -177,23 +204,34 @@ func verifyV1ExactSepolia(p PaymentPayload, r PaymentRequirements) (VerifyResult
 	}
 
 	// Verify the authorization value matches the required amount
-	if p.Authorization.Value != r.Amount {
+	if p.Authorization.Value < r.MaxAmountRequired {
 		return VerifyResult{
 			Valid:  false,
-			Reason: "authorization value does not match required amount",
+			Reason: "authorization value is less than the required maximum amount",
 		}, nil
 	}
 
-	// Verify the authorization to matches the required recipient
-	if p.Authorization.To != r.Recipient {
+	// Verify the authorization to matches the required pay to address
+	if p.Authorization.To != r.PayTo {
 		return VerifyResult{
 			Valid:  false,
-			Reason: "authorization to does not match required recipient",
+			Reason: "authorization to does not match required pay to address",
 		}, nil
 	}
 
 	bigChainID := big.NewInt(11155111) // Sepolia
 	hexChainID := math.HexOrDecimal256(*bigChainID)
+
+	nonceBytes, err := hex.DecodeString(strings.TrimPrefix(p.Authorization.Nonce, "0x"))
+	if err != nil {
+		return VerifyResult{}, utils.NewStatusError(
+			fmt.Errorf("failed to decode nonce: %v", err),
+			http.StatusInternalServerError,
+		)
+	}
+
+	var nonce [32]byte
+	copy(nonce[:], nonceBytes)
 
 	// Construct the typed data
 	typedData := apitypes.TypedData{
@@ -245,36 +283,22 @@ func verifyV1ExactSepolia(p PaymentPayload, r PaymentRequirements) (VerifyResult
 		},
 		PrimaryType: "TransferWithAuthorization",
 		Domain: apitypes.TypedDataDomain{
-			Name:              r.AssetName,
-			Version:           r.AssetVersion,
+			Name:              r.Extra.Name,
+			Version:           r.Extra.Version,
 			ChainId:           &hexChainID,
-			VerifyingContract: r.AssetContract,
+			VerifyingContract: r.Asset,
 		},
 		Message: apitypes.TypedDataMessage{
 			"from":        p.Authorization.From,
 			"to":          p.Authorization.To,
-			"value":       p.Authorization.Value,
-			"validAfter":  p.Authorization.ValidAfter,
-			"validBefore": p.Authorization.ValidBefore,
-			"nonce":       p.Authorization.Nonce,
+			"value":       big.NewInt(p.Authorization.Value),
+			"validAfter":  big.NewInt(p.Authorization.ValidAfter),
+			"validBefore": big.NewInt(p.Authorization.ValidBefore),
+			"nonce":       nonce,
 		},
 	}
 
-	// Parse the signature
-	signature, err := common.ParseHexOrString(p.Signature)
-	if err != nil {
-		return VerifyResult{}, utils.NewStatusError(
-			errors.New("invalid signature"),
-			http.StatusBadRequest,
-		)
-	}
-
-	// Adjust V value (27/28 → 0/1)
-	if signature[64] == 27 || signature[64] == 28 {
-		signature[64] -= 27
-	}
-
-	// Compute EIP-712 hash
+	// Compute the EIP-712 hash
 	domainSeparator, err := typedData.HashStruct("EIP712Domain", typedData.Domain.Map())
 	if err != nil {
 		return VerifyResult{}, utils.NewStatusError(
@@ -283,7 +307,7 @@ func verifyV1ExactSepolia(p PaymentPayload, r PaymentRequirements) (VerifyResult
 		)
 	}
 
-	// Compute Message hash
+	// Compute the Message hash
 	typedDataHash, err := typedData.HashStruct(typedData.PrimaryType, typedData.Message)
 	if err != nil {
 		return VerifyResult{}, utils.NewStatusError(
@@ -296,7 +320,21 @@ func verifyV1ExactSepolia(p PaymentPayload, r PaymentRequirements) (VerifyResult
 	rawData := []byte("\x19\x01" + string(domainSeparator) + string(typedDataHash))
 	sighash := crypto.Keccak256(rawData)
 
-	// Recover public key
+	// Parse the payload signature
+	signature, err := common.ParseHexOrString(p.Signature)
+	if err != nil {
+		return VerifyResult{}, utils.NewStatusError(
+			errors.New("invalid signature"),
+			http.StatusBadRequest,
+		)
+	}
+
+	// Adjust the V value (27/28 → 0/1)
+	if signature[64] == 27 || signature[64] == 28 {
+		signature[64] -= 27
+	}
+
+	// Recover the public key
 	pubkey, err := crypto.Ecrecover(sighash, signature)
 	if err != nil {
 		return VerifyResult{}, utils.NewStatusError(
@@ -305,7 +343,7 @@ func verifyV1ExactSepolia(p PaymentPayload, r PaymentRequirements) (VerifyResult
 		)
 	}
 
-	// Unmarshal public key
+	// Unmarshal the public key
 	recoveredPubkey, err := crypto.UnmarshalPubkey(pubkey)
 	if err != nil {
 		return VerifyResult{}, utils.NewStatusError(
@@ -314,7 +352,7 @@ func verifyV1ExactSepolia(p PaymentPayload, r PaymentRequirements) (VerifyResult
 		)
 	}
 
-	// Convert public key to address
+	// Convert the public key to an address
 	sender := crypto.PubkeyToAddress(*recoveredPubkey)
 
 	// Verify the sender matches the authorization from
@@ -324,8 +362,6 @@ func verifyV1ExactSepolia(p PaymentPayload, r PaymentRequirements) (VerifyResult
 			Reason: "invalid signature",
 		}, nil
 	}
-
-	// TODO: more verification logic...
 
 	// Return valid with no error
 	return VerifyResult{Valid: true}, nil
